2bb3e830c931c7c8e0f20f65bdd454b6b8f1d9a5
junpin <409570250@gapp.fju.edu.tw>
2020-12-08 15:34:12 +0800

    pokercard game

--- src/edu/fju/PokerCard.java
+++ src/edu/fju/PokerCard.java
@@ -1,5 +1,12 @@
 package edu.fju;
 
 public class PokerCard {
+	int value;
+	int point;
+	char type;
+	 
+	public PokerCard(int value) {
+		 this.value = value;
+	 }
 
 }
---
+++ src/edu/fju/poker.java
@@ -0,0 +1,10 @@
+package edu.fju;
+
+public class poker {
+
+	public static void main(String[] args) {
+		PokerCard poker = new PokerCard(12);
+		System.out.println(poker.value);
+	}
+
+}
